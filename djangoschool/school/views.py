b5bab61913739f2f6c7a4dc86d1322bcaf3b768e
Inter Somsith <62319067+somsith201902@users.noreply.github.com>
2020-09-24 14:47:46 +0700

    winter

--- djangoschool/school/views.py
+++ djangoschool/school/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def HomePage(request):
-    return HttpResponse("")
+    # return HttpResponse("<h1>inter</h1>")
+    return render(request, 'school/home.html')
