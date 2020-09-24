3982e9b07afcefbc67afe579ac9736519dc15832
Inter Somsith <62319067+somsith201902@users.noreply.github.com>
2020-09-24 13:30:29 +0700

    Winter

--- djangoschool/school/views.py
+++ djangoschool/school/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render  #ດືງຈາກ template
+from django.http import HttpResponse #ຂຽນເທຶງກະດານເອງ
 
 def HomePage(request):
-    return HttpResponse()
+    return 
